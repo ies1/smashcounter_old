66ca9bbb10ad303537529673157ae7f1f5e93374
ies1 <ies1@bfh.ch>
2019-01-18 13:05:23 +0100

    created resources and so on

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { MainPage } from '..';
 
 /**
  * The Welcome Page is a splash page that quickly describes the app,
@@ -16,11 +17,7 @@ export class WelcomePage {
 
   constructor(public navCtrl: NavController) { }
 
-  login() {
-    this.navCtrl.push('LoginPage');
-  }
-
-  signup() {
-    this.navCtrl.push('SignupPage');
+  go() {
+    this.navCtrl.setRoot(MainPage);
   }
 }
